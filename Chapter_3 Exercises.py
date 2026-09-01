38f0be7dc59191b70e344eefc15493d6ca540aff
rk51500 <rhiannonking81@gmail.com>
2026-08-31 19:08:43 -0500

    Create Chapter_3 Exercises.py
    Things to do at the end of chapter 3 :)

---
+++ Chapter_3 Exercises.py
@@ -0,0 +1,20 @@
+
+print(60 * 60)
+
+
+seconds_per_hour = 60 * 60
+
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+
+print(seconds_per_hour * 24)
+
+
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+
+
+print(seconds_per_day / seconds_per_hour)
+
+
+print(seconds_per_day // seconds_per_hour)
